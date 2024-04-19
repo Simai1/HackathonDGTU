8da1dfda7b5694f28b6edaf86e1db2e3ed95ac87
Artur <artur.tonoyan.04@list.ru>
2024-04-19 21:31:42 +0300

    homePage

--- web/src/App.js
+++ web/src/App.js
@@ -1,7 +1,16 @@
 import "./App.module.scss";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage/HomePage";
 
 function App() {
-  return <div className="App">React</div>;
+  return (
+    <BrowserRouter>
+      <main className="App">React</main>;
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
