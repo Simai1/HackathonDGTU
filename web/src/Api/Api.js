b9280eb2dfc1e76cf35da22d2df3e28aadbb0fd9
Artur <artur.tonoyan.04@list.ru>
2024-04-20 23:24:51 +0300

    COOR

--- web/src/Api/Api.js
+++ web/src/Api/Api.js
@@ -1,9 +1,8 @@
 //? Здесь все запросы к апи, присвоение этих данных состояниями в AssingApiData
 
-import axios from "axios"
+import axios from "axios";
 const server = "http://192.168.43.254:3000";
 
-//! получаем преподов
 export const Login = async (data) => {
   try {
     const response = await axios.post(`${server}/auth/login`, data);
@@ -15,52 +14,61 @@ export const Login = async (data) => {
 };
 
 export const GetDataWarehouse = async () => {
-    try {
-      const response = await axios.get(`${server}/warehouse`);
-      return response.data;
-    } catch (error) {
-      console.error("Error:", error);
-      throw error;
-    }
-  };
+  try {
+    const response = await axios.get(`${server}/warehouse`);
+    return response.data;
+  } catch (error) {
+    console.error("Error:", error);
+    throw error;
+  }
+};
 
-  export const GetdataWarehouse = async () => {
-    try {
-      const response = await axios.get(`${server}/warehouse`);
-      return response;
-    } catch (error) {
-      console.error("Error:", error);
-      throw error;
-    }
-  };
+export const GetdataWarehouse = async () => {
+  try {
+    const response = await axios.get(`${server}/warehouse`);
+    return response;
+  } catch (error) {
+    console.error("Error:", error);
+    throw error;
+  }
+};
 
-  export const GetProductsDataWarehouse = async (data) => {
-    try {
-      const response = await axios.get(`${server}/warehouse/${data}/products`);
-      return response.data;
-    } catch (error) {
-      console.error("Error:", error);
-      throw error;
-    }
-  };
+export const GetProductsDataWarehouse = async (data) => {
+  try {
+    const response = await axios.get(`${server}/warehouse/${data}/products`);
+    return response.data;
+  } catch (error) {
+    console.error("Error:", error);
+    throw error;
+  }
+};
 
-  export const GetdataShop = async () => {
-    try {
-      const response = await axios.get(`${server}/shop`);
-      return response;
-    } catch (error) {
-      console.error("Error:", error);
-      throw error;
-    }
-  };
+export const GetdataShop = async () => {
+  try {
+    const response = await axios.get(`${server}/shop`);
+    return response;
+  } catch (error) {
+    console.error("Error:", error);
+    throw error;
+  }
+};
 
-  export const GetProductsDataShop = async (data) => {
-    try {
-      const response = await axios.get(`${server}/shop/${data}/products`);
-      return response.data;
-    } catch (error) {
-      console.error("Error:", error);
-      throw error;
-    }
-  };
+export const GetProductsDataShop = async (data) => {
+  try {
+    const response = await axios.get(`${server}/shop/${data}/products`);
+    return response.data;
+  } catch (error) {
+    console.error("Error:", error);
+    throw error;
+  }
+};
 
+export const createOffer = async (data) => {
+  try {
+    const response = await axios.post(`${server}/auth/login`, data);
+    return response;
+  } catch (error) {
+    console.error("Error:", error);
+    throw error;
+  }
+};
