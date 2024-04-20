cdb48a93184a8503e17275c691d61b42f595e1cb
RpgGold <101806485+RpgGolden@users.noreply.github.com>
2024-04-20 11:02:59 +0300

    123

--- api/controllers/product.js
+++ api/controllers/product.js
@@ -46,6 +46,8 @@ export default {
             include: {
                 model: Warehouse,
             },
+            order: [['expiryDate', 'DESC']],
+
             where: {
                 expiryDate: {
                     [Op.lte]: fourDaysFromNow,
